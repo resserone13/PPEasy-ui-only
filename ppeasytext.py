613ced4baa9a65837c03ef35d6363fe255173d9f
resserone13 <71921201+resserone13@users.noreply.github.com>
2021-01-18 03:55:50 -0800

    Add files via upload

---
+++ ppeasytext.py
@@ -0,0 +1,73 @@
+
+
+
+#0
+fs_desc = 'Face shields protect the entire face, in particular your eyes, from any splashes or sprays, which along with the nose and mouth can be a gateway for the infectious disease. A face shield may make you less likely to touch your face with unwashed hands. You should ware a face shield while caring for patients. You never know what may cause something to splash towards your face. A face shield are not a replacement for a mask. '  
+
+#1
+level_1_desc ="A level 1 mask provides adequate protection for general short term use. Its primary function is to stop any saliva or mucus from being transferred from person to person. If your mask becomes wet or dirty it is less effective and should be changed. It's not recommended to wear a level 1 mask for long periods of time or while caring for patients."
+
+#2
+level_3_desc = "A level 3 mask provides a higher level of protection than a level 1. Althougth it looks very similar to a level 1 it can be distinguished from a level 1 by having at least 3 layers of material. It also might have a anti-fog stip to help with fogging. A level 3 mask is more fluid resistant than the level one but still should be changed if it becomes wet or dirty. It's ok to ware a level 3 while around patients."
+
+#3
+k_n95_desc = "K-N95 masks are usually thicker and tighter fitting than a level 1 or 3 and provide a higher level of protection. It's recommended for use in patient areas such as the Emergency Dept and patient floors. A K-N95 is NOT fit tested and is NOT the same as a N95."
+
+#4
+n95_desc = "This is a N95 mask. It should be worn while caring for patients undergoing medical procedures that have been deemed as aerosol generating procedures. You should wear the N95 you passed a fit test for."
+
+#5
+papr_desc = "Papr stands for Powered Air Purifying Respirator. It is recommended for use by individuals who have not been fit tested or did not pass a fit test. You should be trained to ensure you properly wear and clean the Papr."
+
+#6
+cloth_desc = 'This is a cloth gown. It must be worn while providing care to patients who are in isolation. A cloth gown differs from a plastic down because it will allow fluids to penetrate the cloth and touch your body. Its NOT recommended to wear a cloth gown while caring for patients with C-diff or TB.'
+
+#7
+plastic_desc = 'This is a plastic gown. Plastic gowns is different from a cloth gown because they are fluid-resistant and impermeable. They will not allow fluids to penetrate the gown and touch your body. It must be worn while providing care to C-Diff patients. Its is recommended to wear a plastic gown while providing care to patients with TB.'
+
+#8
+faq_desc = "Q. What is the difference between a K-N96 and a N95? \n\nA. Both products are said to filter 95 percent of aerosol particulates. K-N95 respirators differ from N95 respirators because they meet the Chinese standard but are not regulated by U.S. agencies. \n\nQ. Do mask really work? \n\nA. Yes. It stops the transmission of saliva and mucus from person to person. \n\nQ. Are hospital really overwhelmed? \n\nA. Yes. Covid patients need to be separated from patients without covid which ties up available rooms and nurses. Also, critical covid patients must be cared for in the ICU.  Hospitals don't have a lot of ICU rooms readily available. If a hospital has 200 Covid patients and only 40 ICU rooms they are easily overwhelmed. \n\nQ. Is now a safe time to travel or visit family? \n\nA. No. Not at all. Traveling can expose you to the virus or allow you to spread the virus. The more you love someone the further you should stay away from them. You do not want to chance spreading the virus to them. \n\nQ. how can I protect myself during the pandemic? \n\nA. Stay away from other people. If you have a necessary reason to go out then you should wear a mask and watch what you touch. Wash your hands as often as possible. Stay separated from people as often as possible."
+
+#9
+dd_desc = "DO wear your mask correctly. Cover you mouth and nose. \n\nDON'T ware your mask improperly. Make sure you mouth and nose is covered.  \n\nDO keep you mask clean. \n\nDON'T touch mask with your hands. \n\nDO change your mask regulary. \n\nDON'T wear a dirty mask' \n\nDO wear your mask while talking. \nDON'T remove your mask to talk. \n\nDO wear masks that fit well and trap your breath. \n\nDON'T ware masks with vents. They allow your breath to escape and spread to others. \n\nDo keep 6 feet apart when out in public even while wearing a mask. \n\nDON'T beark social distancing even if your wearing a mask. \n\nDO"
+
+#10
+covid_desc = 'COVID-19 is caused by a coronavirus called SARS-CoV-2. Older adults and people who have severe underlying medical conditions like heart or lung disease or diabetes seem to be at higher risk for developing more serious complications from COVID-19 illness. \n\nSymptoms may appear 2-14 days after exposure to the virus. People with these symptoms may have COVID-19:\n\n•Fever or chills\n•Cough\n•Shortness of breath or difficulty breathing\n•Fatigue\n•Muscle or body aches\n•Headache\n•New loss of taste or smell\n•Sore throat\n•Congestion or runny nose\n•Nausea or vomiting\n•Diarrhea \n\nEmergency warning signs. If someone is showing any of these signs, seek emergency medical care immediately:\n\n•Trouble breathing\n•Persistent pain or pressure in the chest\n•New confusion\n•Inability to wake or stay awake\n•Bluish lips or face \n\nCOVID-19 most commonly spreads during close contact. People who are physically near (within 6 feet) a person with COVID-19 or have direct contact with that person are at greatest risk of infection. \n\nWhen people with COVID-19 cough, sneeze, sing, talk, or breathe they produce respiratory droplets. These droplets can range in size from larger droplets (some of which are visible) to smaller droplets. Small droplets can also form particles when they dry very quickly in the airstream. \n\nInfections occur mainly through exposure to respiratory droplets when a person is in close contact with someone who has COVID-19. Respiratory droplets cause infection when they are inhaled or deposited on mucous membranes, such as those that line the inside of the nose and mouth. \n\nAs the respiratory droplets travel further from the person with COVID-19, the concentration of these droplets decreases. Larger droplets fall out of the air due to gravity. Smaller droplets and particles spread apart in the air.With passing time, the amount of infectious virus in respiratory droplets also decreases.'
+
+#11
+oxi_desc = 'Oxivir is recommended for general disinfecting. The active ingredient hydrogen peroxide. It is less harsh than most other disinfectants. It has a very short dwell time of just 1 minute. It does NOT kill the bacteria that causes C-Diff and should never be used to disinfect anything that may be contaminated with C-Diff.'
+
+#12
+sani_desc = 'Sani is recommended for general disinfecting. The active ingredient is Alcohol. Be carful when using Alcohol. Inhaling Alcohol vapors can cause nausea, irritation of the nose, throat, and even difficulty with breathing. Alcohol Sani wipes I have a dwell time of 2 minutes \n\nIt does NOT kill the bacteria that causes C-Diff and should never be used to disinfect anything that may be contaminated nwith C-Diff.'
+
+#13
+bleach_desc = 'Bleach is a Sporicidal disinfectant. The active ingredient is Sodium hypochlorite. Bleach kills the bacteria that causes C-Diff and should always be used to disinfect anything that may be contaminated with C-Diff.\n\nNever mix bleach and ammonia. Mixing the to chemicals produces chlorine gas which cam be deadly.'
+
+#14
+ammonia_desc = 'Ammonia is recommended for cleaning sensitive electiral devices. The active ingredient Ammonia. The effective dwell time for ammonia is 3 minutes. \n\nIt does NOT kill the bacteria that causes C-Diff and should never be used to disinfect anything that may be contaminated with C-Diff.\n\nNever mix bleach and ammonia. Mixing the to chemicals produces chlorine gas which cam be deadly.'
+
+#15
+links_btns = [
+    'www.who.int',
+    'www.cdc.gov', 
+    'www.fda.gov', 
+    'www.coronavirus.gov', 
+    'www.covid.cdc.gov', 
+    'www.idsociety.org', 
+    'www.osha.gov'
+    ]
+
+#16
+btn_names=[
+    'Masks', 
+    'Face Shields', 
+    'Gowns', 
+    'Wipes', 
+    'FAQ', 
+    "Dos/Don'ts", 
+    'Covid Info','Links'
+    ]
+
+
+
+info =[fs_desc, level_1_desc, level_3_desc, k_n95_desc, n95_desc, papr_desc, cloth_desc, plastic_desc, faq_desc, dd_desc, covid_desc, oxi_desc, sani_desc, bleach_desc, ammonia_desc, links_btns, btn_names]
